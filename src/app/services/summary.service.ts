928e32ddc6b467eb509cce4d90d4dbdbd231b797
Dolan <dolan_miu@hotmail.com>
2017-07-22 23:02:52 +0100

    Work in progress

--- src/app/services/summary.service.ts
+++ src/app/services/summary.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 import { ActivitiesService } from './activities/activities.service';
 import { PatientsService } from './patients/patients.service';
@@ -7,7 +8,17 @@ import { PatientsService } from './patients/patients.service';
 export class SummaryService {
 
     constructor(private patientsService: PatientsService, private activitiesService: ActivitiesService) {
+        // this.patientsService.fetchPatients()
+        // .flatMap((patients) => {
+        //     const patients$: Observable[] = [];
+        //     patients.forEach((patient) => {
 
+        //     });
+
+        //     Observable.forkJoin(patients$)
+
+        //     return
+        // })
     }
 
 }
