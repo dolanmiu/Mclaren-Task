3128710e8e5cc2f1f2bf3d39d6de0c84bb90976d
Dolan <dolan_miu@hotmail.com>
2017-07-22 20:23:22 +0100

    Add more tests

--- src/app/services/patients/patients.service.spec.ts
+++ src/app/services/patients/patients.service.spec.ts
@@ -1,5 +1,6 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { HttpModule } from '@angular/http';
+import 'rxjs/Rx';
 
 import { PatientsService } from './patients.service';
 
@@ -17,6 +18,28 @@ describe('PatientsService', () => {
 
     it('should get all patients', inject([PatientsService], (service: PatientsService) => {
         service.fetchPatients().subscribe((patients) => {
+            expect(patients).toBeDefined();
+            expect(patients).toBeTruthy();
+            expect(patients).toEqual(jasmine.any(Array));
+            expect(patients.length).toBeGreaterThan(0);
+        });
+    }));
+
+    it('should get a specific patient summary', inject([PatientsService], (service: PatientsService) => {
+        service.fetchPatientSummary(1).subscribe((patients) => {
+            console.log(patients);
+            expect(patients).toBeDefined();
+            expect(patients).toBeTruthy();
+            expect(patients).toEqual(jasmine.any(Array));
+            expect(patients.length).toBeGreaterThan(0);
+        }, (err) => {
+            console.log(err);
+        });
+    }));
+
+    it('should throw an error if invalid id for specific patient summary', inject([PatientsService], (service: PatientsService) => {
+        service.fetchPatientSummary(-1).subscribe((patients) => {
+            console.log(patients);
             expect(patients).toBeDefined();
             expect(patients).toBeTruthy();
             expect(patients).toEqual(jasmine.any(Array));
